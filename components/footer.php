adf9534d7d8a8f1945f59250eba4ed58c6fc8db0
EsauProgramando <eaveintemillap@alumno.unsm.edu.pe>
2023-05-25 16:16:28 -0500

    nn

--- components/footer.php
+++ components/footer.php
@@ -1,52 +1,52 @@
 <footer class="footer">
 
-   <div class="container">
-      <div class="sec aboutus">
-         <h2>About us</h2>
-         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis hic est deleniti quos suscipit dolorem labore nihil saepe reiciendis laborum officia illum, maiores voluptatem sapiente cupiditate? Quas ullam molestiae mollitia?</p>
-         
-         <ul class="sci">
-         <li><a href="#"><i class="fa-brands fa-facebook"></i></a></li>
-         <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
-         <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
-         <li><a href="#"><i class="fa-brands fa-youtube"></i></a></li>
+  <div class="container">
+    <div class="sec aboutus">
+      <h2>About us</h2>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis hic est deleniti quos suscipit dolorem labore nihil saepe reiciendis laborum officia illum, maiores voluptatem sapiente cupiditate? Quas ullam molestiae mollitia?</p>
+
+      <ul class="sci">
+        <li><a href="#"><i class="fa-brands fa-facebook"></i></a></li>
+        <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
+        <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+        <li><a href="#"><i class="fa-brands fa-youtube"></i></a></li>
       </ul>
-      </div>
-      <div class="sec quickLinks">
-         <h2>Quick Links</h2>
-         <ul>
-            <li><a href="home.php">Principal</a></li>
-            <li><a href="about.php">Nosotros</a></li>
-            <li><a href="menu.php">Variedades</a></li>
-            <li><a href="orders.php">Ordenes</a></li>
-            <li><a href="contact.php">contacto</a></li>
-         </ul>
-      </div>
-      <div class="sec contact">
-         <h2>Contacto</h2>
-         <ul class="info">
-            <li>
-               <span><i class="fa-solid fa-location-dot"></i></span>
-               <span>Morales Jr ---<br>
-               San Martín
-               </span>
-            </li>
-            <li>
-               <span><i class="fa-solid fa-phone"></i></span>
-               <p><a href="tel:4535131">35115151</a></p>
-            </li>
-            <li>
-               <span><i class="fa-solid fa-location-dot"></i></span>
-               <p><a href="mailto:correo@gmail.com">correo@gmail.com</a></p>
-            </li>
-         </ul>
-      </div>
-      
-   </div>
-   
+    </div>
+    <div class="sec quickLinks">
+      <h2>Quick Links</h2>
+      <ul>
+        <li><a href="home.php">Principal</a></li>
+        <li><a href="about.php">Nosotros</a></li>
+        <li><a href="menu.php">Variedades</a></li>
+        <li><a href="orders.php">Ordenes</a></li>
+        <li><a href="contact.php">contacto</a></li>
+      </ul>
+    </div>
+    <div class="sec contact">
+      <h2>Contacto</h2>
+      <ul class="info">
+        <li>
+          <span><i class="fa-solid fa-location-dot"></i></span>
+          <span>Cacatachi<br>
+            Jr. Independencia #643
+          </span>
+        </li>
+        <li>
+          <span><i class="fa-solid fa-phone"></i></span>
+          <p><a href="tel:990431916">990431916</a> - <a href="tel:949804737">949804737</a></p>
+        </li>
+        <li>
+          <span><i class="fa-sharp fa-regular fa-envelope"></i></span>
+          <p><a href="mailto:Wingaflores@gmail.com">Wingaflores@gmail.com</a></p>
+        </li>
+      </ul>
+    </div>
+
+  </div>
+
 
 </footer>
 <div class="credit">&copy; copyright @ <?= date('Y'); ?> por <span>UNSM</span> | todos los derechos reservados!</div>
 <div class="loader">
-   <img src="images/jugo.gif" alt="">
+  <img src="images/jugo.gif" alt="">
 </div>
